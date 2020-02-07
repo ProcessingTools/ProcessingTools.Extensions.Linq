a2cc15bcf2f7be6c358867f7f64b18956d299d9a
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2020-02-07 14:38:37 +0200

    Add documentation

--- ProcessingTools.Extensions.Linq/ProcessingTools.Extensions.Linq/Dynamic/DynamicQueryable.cs
+++ ProcessingTools.Extensions.Linq/ProcessingTools.Extensions.Linq/Dynamic/DynamicQueryable.cs
@@ -78,11 +78,16 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             }
         }
 
+        /// <summary>
+        /// Determines whether a sequence contains any elements.
+        /// </summary>
+        /// <param name="source">A sequence to check for being empty.</param>
+        /// <returns>True if the source sequence contains any elements; otherwise, False.</returns>
         public static bool Any(this IQueryable source)
         {
             if (source is null)
             {
-                throw new ArgumentNullException(nameof(source));
+                return false;
             }
 
             return (bool)source.Provider.Execute(
@@ -93,16 +98,27 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
                     source.Expression));
         }
 
+        /// <summary>
+        /// Determines whether a sequence contains any elements.
+        /// </summary>
+        /// <param name="source">A sequence to check for being empty.</param>
+        /// <returns>True if the source sequence contains any elements; otherwise, False.</returns>
         public static bool Any(this IEnumerable source)
         {
             if (source is null)
             {
-                throw new ArgumentNullException(nameof(source));
+                return false;
             }
 
             return source.AsQueryable().Any();
         }
 
+        /// <summary>
+        /// Returns the number of elements in a sequence.
+        /// </summary>
+        /// <param name="source">The <see cref="IQueryable"/> that contains the elements to be counted.</param>
+        /// <returns>The number of elements in the input sequence.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static int Count(this IQueryable source)
         {
             if (source is null)
@@ -118,6 +134,12 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
                     source.Expression));
         }
 
+        /// <summary>
+        /// Returns the number of elements in a sequence.
+        /// </summary>
+        /// <param name="source">The <see cref="IEnumerable"/> that contains the elements to be counted.</param>
+        /// <returns>The number of elements in the input sequence.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static int Count(this IEnumerable source)
         {
             if (source is null)
@@ -128,6 +150,12 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().Count();
         }
 
+        /// <summary>
+        /// Returns distinct elements from a sequence by using the default equality comparer to compare values.
+        /// </summary>
+        /// <param name="source">The <see cref="IQueryable"/> to remove duplicates from.</param>
+        /// <returns>An <see cref="IQueryable"/> that contains distinct elements from source.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static IQueryable Distinct(this IQueryable source)
         {
             if (source is null)
@@ -143,6 +171,12 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
                     source.Expression));
         }
 
+        /// <summary>
+        /// Returns distinct elements from a sequence by using the default equality comparer to compare values.
+        /// </summary>
+        /// <param name="source">The <see cref="IEnumerable"/> to remove duplicates from.</param>
+        /// <returns>An <see cref="IEnumerable"/> that contains distinct elements from source.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static IEnumerable Distinct(this IEnumerable source)
         {
             if (source is null)
@@ -153,6 +187,15 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().Distinct();
         }
 
+        /// <summary>
+        /// Groups the elements of a sequence according to a specified key selector function.
+        /// </summary>
+        /// <param name="source">An <see cref="IQueryable"/> whose elements to group.</param>
+        /// <param name="keySelector">A function to extract the key for each element.</param>
+        /// <param name="elementSelector">A function to map each source element to an element in an <see cref="IQueryable"/>.</param>
+        /// <param name="values">Values for selection.</param>
+        /// <returns>An <see cref="IQueryable"/> of the grouping.</returns>
+        /// <exception cref="ArgumentNullException">Source or keySelector or elementSelector is null.</exception>
         public static IQueryable GroupBy(this IQueryable source, string keySelector, string elementSelector, params object[] values)
         {
             if (source is null)
@@ -182,6 +225,15 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
                     Expression.Quote(elementLambda)));
         }
 
+        /// <summary>
+        /// Groups the elements of a sequence according to a specified key selector function.
+        /// </summary>
+        /// <param name="source">An <see cref="IEnumerable"/> whose elements to group.</param>
+        /// <param name="keySelector">A function to extract the key for each element.</param>
+        /// <param name="elementSelector">A function to map each source element to an element in an <see cref="IEnumerable"/>.</param>
+        /// <param name="values">Values for selection.</param>
+        /// <returns>An <see cref="IEnumerable"/> of the grouping.</returns>
+        /// <exception cref="ArgumentNullException">Source or keySelector or elementSelector is null.</exception>
         public static IEnumerable GroupBy(this IEnumerable source, string keySelector, string elementSelector, params object[] values)
         {
             if (source is null)
@@ -192,11 +244,28 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().GroupBy(keySelector, elementSelector, values);
         }
 
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending or descending order according to a key.
+        /// </summary>
+        /// <typeparam name="T">Type of the <see cref="IQueryable"/> element.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="ordering">Ordering expression.</param>
+        /// <param name="values">Values for the expression.</param>
+        /// <returns>An ordered <see cref="IQueryable"/> whose elements are sorted according to an expression.</returns>
+        /// <exception cref="ArgumentNullException">Source or ordering is null.</exception>
         public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string ordering, params object[] values)
         {
             return (IQueryable<T>)OrderBy((IQueryable)source, ordering, values);
         }
 
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending or descending order according to a key.
+        /// </summary>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="ordering">Ordering expression.</param>
+        /// <param name="values">Values for the expression.</param>
+        /// <returns>An ordered <see cref="IQueryable"/> whose elements are sorted according to an expression.</returns>
+        /// <exception cref="ArgumentNullException">Source or ordering is null.</exception>
         public static IQueryable OrderBy(this IQueryable source, string ordering, params object[] values)
         {
             if (source is null)
@@ -232,6 +301,15 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.Provider.CreateQuery(queryExpression);
         }
 
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending or descending order according to a key.
+        /// </summary>
+        /// <typeparam name="T">Type of the <see cref="IEnumerable"/> element.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="ordering">Ordering expression.</param>
+        /// <param name="values">Values for the expression.</param>
+        /// <returns>An ordered <see cref="IEnumerable"/> whose elements are sorted according to an expression.</returns>
+        /// <exception cref="ArgumentNullException">Source or ordering is null.</exception>
         public static IEnumerable<T> OrderBy<T>(this IEnumerable<T> source, string ordering, params object[] values)
         {
             if (source is null)
@@ -242,6 +320,14 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().OrderBy(ordering, values);
         }
 
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending or descending order according to a key.
+        /// </summary>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="ordering">Ordering expression.</param>
+        /// <param name="values">Values for the expression.</param>
+        /// <returns>An ordered <see cref="IEnumerable"/> whose elements are sorted according to an expression.</returns>
+        /// <exception cref="ArgumentNullException">Source or ordering is null.</exception>
         public static IEnumerable OrderBy(this IEnumerable source, string ordering, params object[] values)
         {
             if (source is null)
@@ -252,6 +338,14 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().OrderBy(ordering, values);
         }
 
+        /// <summary>
+        /// Projects each element of a sequence into a new form by incorporating the element's index.
+        /// </summary>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="selector">A projection expression to apply to each element.</param>
+        /// <param name="values">Values for the projection expression.</param>
+        /// <returns>An <see cref="IQueryable"/> whose elements are the result of invoking a projection expression on each element of source.</returns>
+        /// <exception cref="ArgumentNullException">Source or selector is null.</exception>
         public static IQueryable Select(this IQueryable source, string selector, params object[] values)
         {
             if (source is null)
@@ -275,6 +369,14 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
                     Expression.Quote(lambda)));
         }
 
+        /// <summary>
+        /// Projects each element of a sequence into a new form by incorporating the element's index.
+        /// </summary>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="selector">A projection expression to apply to each element.</param>
+        /// <param name="values">Values for the projection expression.</param>
+        /// <returns>An <see cref="IEnumerable"/> whose elements are the result of invoking a projection expression on each element of source.</returns>
+        /// <exception cref="ArgumentNullException">Source or selector is null.</exception>
         public static IEnumerable Select(this IEnumerable source, string selector, params object[] values)
         {
             if (source is null)
@@ -285,6 +387,13 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().Select(selector, values);
         }
 
+        /// <summary>
+        /// Bypasses a specified number of elements in a sequence and then returns the remaining elements.
+        /// </summary>
+        /// <param name="source">An <see cref="IQueryable"/> to return elements from.</param>
+        /// <param name="count">The number of elements to skip before returning the remaining elements.</param>
+        /// <returns>An <see cref="IQueryable"/> that contains elements that occur after the specified index in the input sequence.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static IQueryable Skip(this IQueryable source, int count)
         {
             if (source is null)
@@ -302,6 +411,13 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.Provider.CreateQuery(expression);
         }
 
+        /// <summary>
+        /// Bypasses a specified number of elements in a sequence and then returns the remaining elements.
+        /// </summary>
+        /// <param name="source">An <see cref="IEnumerable"/> to return elements from.</param>
+        /// <param name="count">The number of elements to skip before returning the remaining elements.</param>
+        /// <returns>An <see cref="IEnumerable"/> that contains elements that occur after the specified index in the input sequence.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static IEnumerable Skip(this IEnumerable source, int count)
         {
             if (source is null)
@@ -312,6 +428,13 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().Skip(count);
         }
 
+        /// <summary>
+        /// Returns a specified number of contiguous elements from the start of a sequence.
+        /// </summary>
+        /// <param name="source">The sequence to return elements from.</param>
+        /// <param name="count">The number of elements to return.</param>
+        /// <returns>An <see cref="IQueryable"/> that contains the specified number of elements from the start of source.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static IQueryable Take(this IQueryable source, int count)
         {
             if (source is null)
@@ -329,6 +452,13 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.Provider.CreateQuery(expression);
         }
 
+        /// <summary>
+        /// Returns a specified number of contiguous elements from the start of a sequence.
+        /// </summary>
+        /// <param name="source">The sequence to return elements from.</param>
+        /// <param name="count">The number of elements to return.</param>
+        /// <returns>An <see cref="IEnumerable"/> that contains the specified number of elements from the start of source.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
         public static IEnumerable Take(this IEnumerable source, int count)
         {
             if (source is null)
@@ -339,11 +469,28 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().Take(count);
         }
 
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">An <see cref="IQueryable{T}"/> to filter.</param>
+        /// <param name="predicate">An expression to test each element for a condition.</param>
+        /// <param name="values">Values for the predicate.</param>
+        /// <returns>An <see cref="IQueryable{T}"/> that contains elements from the input sequence that satisfy the condition specified by predicate.</returns>
+        /// <exception cref="ArgumentNullException">Source or predicate is null.</exception>
         public static IQueryable<T> Where<T>(this IQueryable<T> source, string predicate, params object[] values)
         {
             return (IQueryable<T>)Where((IQueryable)source, predicate, values);
         }
 
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <param name="source">An <see cref="IQueryable"/> to filter.</param>
+        /// <param name="predicate">An expression to test each element for a condition.</param>
+        /// <param name="values">Values for the predicate.</param>
+        /// <returns>An <see cref="IQueryable"/> that contains elements from the input sequence that satisfy the condition specified by predicate.</returns>
+        /// <exception cref="ArgumentNullException">Source or predicate is null.</exception>
         public static IQueryable Where(this IQueryable source, string predicate, params object[] values)
         {
             if (source is null)
@@ -367,6 +514,15 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
                     Expression.Quote(lambda)));
         }
 
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> to filter.</param>
+        /// <param name="predicate">An expression to test each element for a condition.</param>
+        /// <param name="values">Values for the predicate.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> that contains elements from the input sequence that satisfy the condition specified by predicate.</returns>
+        /// <exception cref="ArgumentNullException">Source or predicate is null.</exception>
         public static IEnumerable<T> Where<T>(this IEnumerable<T> source, string predicate, params object[] values)
         {
             if (source is null)
@@ -377,6 +533,14 @@ namespace ProcessingTools.Extensions.Linq.Dynamic
             return source.AsQueryable().Where(predicate, values);
         }
 
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <param name="source">An <see cref="IEnumerable"/> to filter.</param>
+        /// <param name="predicate">An expression to test each element for a condition.</param>
+        /// <param name="values">Values for the predicate.</param>
+        /// <returns>An <see cref="IEnumerable"/> that contains elements from the input sequence that satisfy the condition specified by predicate.</returns>
+        /// <exception cref="ArgumentNullException">Source or predicate is null.</exception>
         public static IEnumerable Where(this IEnumerable source, string predicate, params object[] values)
         {
             if (source is null)
